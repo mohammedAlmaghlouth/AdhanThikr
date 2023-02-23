2735c8178356ed59c315c60e3d6bf30e27d02d14
Mohammed Al Maghlouth <65549274+mohammedAlmaghlouth@users.noreply.github.com>
2023-02-24 00:31:48 +0300

    LR3

--- lib/main.dart
+++ lib/main.dart
@@ -42,8 +42,40 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           child: const Text('Go to Counter Page'),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: ((context) => const CounterPage()),
+              ),
+            );
+          },
         ),
+      ),
+    );
+  }
+}
+
+class CounterPage extends ConsumerWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int counter = ref.watch(counterProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: Center(
+        child: Text(
+          counter.toString(),
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+        },
       ),
     );
   }
